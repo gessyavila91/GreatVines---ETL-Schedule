884ce7fd128d77e32226518a8f646531058bed6d
GetseAvila <getsemani.avila@claseazul.com>
2023-09-11 09:02:35 -0600

    refactor(sftp) move sftp to new class

--- GreatVines - ETL Schedule/Program.cs
+++ GreatVines - ETL Schedule/Program.cs
@@ -8,61 +8,8 @@ namespace SftpExample
     {
         static void Main(string[] args)
         {
-            string host = "sftp.greatvines.com";
-            int port = 22; // El puerto por defecto para SFTP es 22
-            string username = "claseazul@greatvines.com";
-            string password = "85NGAOtVxDMqkQMWJKsVPXKKe";
-            string remoteDirectory = "/";
             
-            string localFilePath = @"C:\archivo_a_subir.zip";
-
-            
-            using (var sftp = new SftpClient(host, port, username, password))
-            {
-                sftp.Connect();
-                
-                listFiles(sftp, remoteDirectory);
-                downloadFile(sftp, remoteDirectory);
-                UploadFile(sftp, localFilePath, remoteDirectory);
-
-                sftp.Disconnect();
-            }
-
-            Console.WriteLine("¡Terminado!");
-        }
-        private static void listFiles(SftpClient sftp, string remoteDirectory) {
-
-            // Listar archivos del directorio
-            var files = sftp.ListDirectory(remoteDirectory);
-            foreach (var file in files) {
-                Console.WriteLine(file.Name);
-            }
-        }
-        private static void downloadFile(SftpClient sftp, string remoteDirectory) {
-
-            // Descargar un archivo
-            string remoteFileName = "nombre_del_archivo.txt";
-            string localPath = @"C:\ruta\local\del\archivo.txt";
-            using (var fileStream = new FileStream(localPath, FileMode.Create)) {
-                sftp.DownloadFile(remoteDirectory + remoteFileName, fileStream);
-            }
         }
         
-        private static void UploadFile(SftpClient sftp, string localFilePath, string remoteDirectory)
-        {
-            sftp.Connect();
-
-            // Si el directorio remoto no existe, lo crea.
-            if (!sftp.Exists(remoteDirectory))
-            {
-                sftp.CreateDirectory(remoteDirectory);
-            }
-
-            using (var fileStream = new FileStream(localFilePath, FileMode.Open))
-            {
-                sftp.UploadFile(fileStream, remoteDirectory + Path.GetFileName(localFilePath));
-            }
-            
-        }
     }
 }
---
+++ GreatVines - ETL Schedule/sftp.cs
@@ -0,0 +1,62 @@
+﻿namespace SftpExample;
+
+using System;
+using System.IO;
+using Renci.SshNet;
+
+
+public class sftp {
+    string host = "sftp.greatvines.com";
+    int port = 22;// El puerto por defecto para SFTP es 22
+    string username = "claseazul@greatvines.com";
+    string password = "85NGAOtVxDMqkQMWJKsVPXKKe";
+    string remoteDirectory = "/";
+
+    string localFilePath = @"C:\archivo_a_subir.zip";
+
+    public void sftpTEST() {
+        using (var sftp = new SftpClient(host, port, username, password)) {
+            sftp.Connect();
+
+            listFiles(sftp, remoteDirectory);
+            downloadFile(sftp, remoteDirectory);
+            UploadFile(sftp, localFilePath, remoteDirectory);
+
+            sftp.Disconnect();
+        }
+
+        Console.WriteLine("¡Terminado!");
+    }
+    private void listFiles(SftpClient sftp, string remoteDirectory) {
+
+        // Listar archivos del directorio
+        var files = sftp.ListDirectory(remoteDirectory);
+        foreach (var file in files) {
+            Console.WriteLine(file.Name);
+        }
+    }
+    private void downloadFile(SftpClient sftp, string remoteDirectory) {
+
+        // Descargar un archivo
+        string remoteFileName = "nombre_del_archivo.txt";
+        string localPath = @"C:\ruta\local\del\archivo.txt";
+        using (var fileStream = new FileStream(localPath, FileMode.Create)) {
+            sftp.DownloadFile(remoteDirectory + remoteFileName, fileStream);
+        }
+        Console.WriteLine("¡Descarga Terminada!");
+    }
+
+    private void UploadFile(SftpClient sftp, string localFilePath, string remoteDirectory) {
+        sftp.Connect();
+
+        // Si el directorio remoto no existe, lo crea.
+        if (!sftp.Exists(remoteDirectory)) {
+            sftp.CreateDirectory(remoteDirectory);
+        }
+
+        using (var fileStream = new FileStream(localFilePath, FileMode.Open)) {
+            sftp.UploadFile(fileStream, remoteDirectory + Path.GetFileName(localFilePath));
+        }
+
+    }
+}
