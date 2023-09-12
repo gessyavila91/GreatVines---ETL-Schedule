d2e02d7e0ae8f8d3ce8641c3a452abf9b2b52060
GetseAvila <getsemani.avila@claseazul.com>
2023-09-11 21:50:15 -0600

    feat(sql): adjust database connection handling and data insertion methods
    This commit modifies the `SqlServer_Helper` class enhancing database connection functionality with a fallback option and implements methods for data insertion into the database. An ErrorHandler was introduced for accurate error logging to a text file. A test project `GreatVinesTest` was added to the solution for unit testing ensuring the robustness of our ETL process and new packages were included for effective configuration management and effortless testing.

---
+++ GreatVines - ETL Schedule/helper/ErrorHandler_Helper.cs
@@ -0,0 +1,49 @@
+﻿namespace GreatVibesSchedule.helper;
+
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+
+public class ErrorHandler_Helper {
+    
+    private static string logFilePath = @"log.txt";
+    
+    public static void HandleException(Exception ex, string className, string functionName) {
+        LogErrorToFile(ex, className, functionName);
+        throw ex;
+    }
+
+    private static void LogErrorToFile(Exception ex, string className, string functionName) {
+        
+        using StreamWriter writer = new StreamWriter(logFilePath, append: true);
+
+        writer.WriteLine("---------------------------------------------------------");
+        writer.WriteLine($"Date/Time: {DateTime.Now}");
+        writer.WriteLine($"Class: {className}, Function: {functionName}");
+        writer.WriteLine($"Error source: {ex.Source}");
+        writer.WriteLine($"Error message: {ex.Message}");
+        writer.WriteLine($"Stack trace: {ex.StackTrace}");
+        writer.WriteLine("---------------------------------------------------------");
+    }
+
+    private static void LogErrorToDatabase(Exception ex, string className, string methodName) {
+        string connectionString = "";// Tu cadena de conexión aquí
+
+        string query = @"INSERT INTO ErrorLogs(Date, ClassName, MethodName, ErrorMessage, StackTrace)
+                         VALUES (@Date, @ClassName, @MethodName, @ErrorMessage, @StackTrace)";
+
+        using SqlConnection connection = SqlServer_Helper.GetDbConnection();
+        using SqlCommand command = new SqlCommand(query, connection);
+
+        command.Parameters.AddWithValue("@Date", DateTime.Now);
+        command.Parameters.AddWithValue("@ClassName", className);
+        command.Parameters.AddWithValue("@MethodName", methodName);
+        command.Parameters.AddWithValue("@ErrorMessage", ex.Message);
+        command.Parameters.AddWithValue("@StackTrace", ex.StackTrace);
+
+        connection.Open();
+
+        command.ExecuteNonQuery();
+    }
+}
--- GreatVines - ETL Schedule/helper/SqlServer_Helper.cs
+++ GreatVines - ETL Schedule/helper/SqlServer_Helper.cs
@@ -1,53 +1,105 @@
-﻿namespace GreatVibesSchedule.helper;
+﻿using System.Reflection;
+namespace GreatVibesSchedule.helper;
 
 using System;
 using System.Data;
 using System.Data.SqlClient;
 using System.Text;
 public static class SqlServer_Helper {
-    private static string BuildConnectionString() {
-        SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder {
-            DataSource = "192.168.1.21",
-            InitialCatalog = "SBO_Tequilaspremium_Prod",
-            UserID = "Desarrollos",
-            Password = "D3v5ll0@7r@22$"
-        };
+    private static string getConnectionString(String database = null) {
+        SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+        try {
+            builder.DataSource = "192.168.1.21";
+            builder.InitialCatalog = database ?? "SBO_Tequilaspremium_Prod";
+            builder.UserID = "Desarrollos";
+            builder.Password = "D3v5ll0@7r@22$";
+        } catch (Exception ex) {
+            ErrorHandler_Helper.HandleException(ex, MethodBase.GetCurrentMethod().DeclaringType.Name, MethodBase.GetCurrentMethod().Name);
+        }
+        
         return builder.ToString();
+    }
+    public static SqlConnection GetDbConnection(String database = null) {
+        SqlConnection connection = null;
+
+        try {
+            connection = new SqlConnection(getConnectionString(database));
+        } catch (Exception ex) {
+            ErrorHandler_Helper.HandleException(ex, MethodBase.GetCurrentMethod().DeclaringType.Name, MethodBase.GetCurrentMethod().Name);
+        }
+
+        return connection;
     }
 
     public static DataTable ExecuteStoredProcedure(string storedProcedureName, SqlParameter[] parameters = null) {
-        string connectionString = BuildConnectionString();
+        string connectionString = getConnectionString();
         DataTable result = new DataTable();
 
-        using (SqlConnection connection = new SqlConnection(connectionString)) {
-            using (SqlCommand cmd = new SqlCommand(storedProcedureName, connection)) {
-                cmd.CommandType = CommandType.StoredProcedure;
+        try {
+            using (SqlConnection connection = new SqlConnection(connectionString)) {
+                using (SqlCommand cmd = new SqlCommand(storedProcedureName, connection)) {
+                    cmd.CommandType = CommandType.StoredProcedure;
 
-                if (parameters != null) {
-                    cmd.Parameters.AddRange(parameters);
-                }
+                    if (parameters != null) {
+                        cmd.Parameters.AddRange(parameters);
+                    }
 
-                using (SqlDataAdapter adapter = new SqlDataAdapter(cmd)) {
-                    adapter.Fill(result);
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(cmd)) {
+                        adapter.Fill(result);
+                    }
                 }
             }
+        } catch (Exception ex) {
+            ErrorHandler_Helper.HandleException(ex, MethodBase.GetCurrentMethod().DeclaringType.Name,
+                MethodBase.GetCurrentMethod().Name);
         }
 
         return result;
     }
 
     public static DataTable ExecuteQuery(string query) {
-        using SqlConnection connection = new SqlConnection(BuildConnectionString());
+        using SqlConnection connection = GetDbConnection();
         using SqlCommand command = new SqlCommand(query, connection);
+        DataTable result = new DataTable();
 
-        connection.Open();
-
-        using SqlDataReader reader = command.ExecuteReader();
-
-        var result = new DataTable();
-        result.Load(reader);
+        try {
+            connection.Open();
+            using SqlDataReader reader = command.ExecuteReader();
+            result.Load(reader);
+            connection.Close();
+        } catch (Exception ex) {
+            ErrorHandler_Helper.HandleException(ex, MethodBase.GetCurrentMethod().DeclaringType.Name,
+                MethodBase.GetCurrentMethod().Name);
+        }
 
         return result;
+    }
+
+    public static void InsertIntoDatabase(string tableName, params SqlParameter[] parameters) {
+        StringBuilder query = new StringBuilder($"INSERT INTO {tableName} (");
+        for (int i = 0; i < parameters.Length; i++) {
+            query.Append(parameters[i].ParameterName.TrimStart('@'));
+            if (i < parameters.Length - 1)
+                query.Append(", ");
+        }
+        query.Append(") VALUES (");
+        for (int i = 0; i < parameters.Length; i++) {
+            query.Append(parameters[i].ParameterName);
+            if (i < parameters.Length - 1)
+                query.Append(", ");
+        }
+        query.Append(")");
+        
+        using (SqlConnection connection = new SqlConnection(getConnectionString())) {
+            connection.Open();
+            
+            using (SqlCommand command = new SqlCommand(query.ToString(), connection)) {
+                command.Parameters.AddRange(parameters);
+                
+                command.ExecuteNonQuery();
+                Console.WriteLine("El registro se ha insertado de manera exitosa en la base de datos.");
+            }
+        }
     }
 
 }
